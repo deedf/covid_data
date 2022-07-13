f7873417c195ba34a0d47e85144446869c813d93
Didier Frick <didier@dfr.ch>
2022-07-13 15:10:46 +0200

    update to use Pandas and ckan

--- graph1.py
+++ graph1.py
@@ -4,33 +4,30 @@ Make symptoms graph.
 """
 import logging
 from collections import namedtuple
-from datetime import date
+from datetime import date, datetime
+from io import StringIO
 from pathlib import Path
 from typing import Any, Dict, List, Tuple
-from datetime import datetime
 
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
+import pandas as pd
 import requests
 from cachecontrol import CacheControl
 from cachecontrol.caches import FileCache
+from ckanapi import RemoteCKAN
+from matplotlib.patches import Rectangle
 from requests import Response
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-try:
-    cached_sess = CacheControl(
-        requests.Session(), cache=FileCache(Path.home() / ".webcache")
-    )
-except Exception as e:  # pylint: disable=broad-except
-    logger.debug(e)
-
-PACKAGE_NAME="covid-19-schweiz"
-
-PACKAGE_URL = (
-    f"https://ckan.opendata.swiss/api/3/action/package_show"
+cached_sess = CacheControl(
+    requests.Session(), cache=FileCache(Path.home() / ".webcache")
 )
+
+
+DATASET_NAME = "covid-19-schweiz"
+CKAN_API_URL = "https://ckan.opendata.swiss/"
 
 START_DATE = date(2021, 3, 23)
 END_DATE = date(2022, 4, 5)
@@ -40,7 +37,7 @@ SYMPTOM_COLOR = (0.1, 0.8, 0.2)
 DEATH_UNKNOWN_COLOR = (0.5, 0.5, 0.5)
 HOSP_UNKNOWN_COLOR = (0.6, 0.6, 0.6)
 SYMPTOM_UNKNOWN_COLOR = (0.7, 0.7, 0.7)
-TRANSPARENT_COLOR = (0,0,0,0)
+TRANSPARENT_COLOR = (0, 0, 0, 0)
 
 AGE_BAR_DICT = {
     "0 - 9": (0, 9),
@@ -62,51 +59,46 @@ AGE_BAR_DICT = {
     "75+": (75, 89),
     "unknown": (90, 99),
 }
+
 CovidData = namedtuple("CovidData", ["hosp", "death", "symptoms"])
 
 
-def _get_package(id: str) -> Dict[str, Any]:
-    result = cached_sess.get(PACKAGE_URL,params={'id':id})
+def _get_data(resource: Dict[str, Any]) -> pd.DataFrame:
+    result: Response = cached_sess.get(resource["download_url"])
     result.raise_for_status()
-    return result.json()["result"]
-
-
-def _get_data(
-    package_url: str, date_attr: str, cutoff_date: str
-) -> List[Dict[str, str]]:
-    result: Response = cached_sess.get(package_url)
-    result.raise_for_status()
-    return [
-        i
-        for i in result.json()
-        if str(i[date_attr]) == cutoff_date and i["geoRegion"] == "CHFL"
-    ]
+    return pd.read_csv(StringIO(result.text))
 
 
 def _get_all_data(
     death: Dict[str, Any], hosp: Dict[str, Any], symptoms: Dict[str, Any], at_date: date
 ) -> CovidData:
     iso_calendar = date.isocalendar(at_date)
-    date_iso = str(iso_calendar[0]) + str(iso_calendar[1])
-    death_data = _get_data(death["download_url"], "datum", date_iso)
-    hosp_data = _get_data(hosp["download_url"], "datum", date_iso)
-    symptom_data = _get_data(symptoms["download_url"], "date", at_date.isoformat())
-    hosp_count = dict((i["altersklasse_covid19"], i["sumTotal"]) for i in hosp_data)
-    death_count = dict((i["altersklasse_covid19"], i["sumTotal"]) for i in death_data)
-    symptom_count = dict(
-        [
-            (i["age_group"], i["sumTotal"])
-            for i in symptom_data
-            if i["vaccine"] == "all"
-            and i["age_group"] != "all"
-            and i["severity"] == "all"
-        ]
+    date_iso = int(str(iso_calendar[0]) + str(iso_calendar[1]))
+    p = _get_data(death)
+    death_data = (
+        p[(p["datum"] == date_iso) & (p["geoRegion"] == "CHFL")]
+        .set_index("altersklasse_covid19")["sumTotal"]
+        .groupby(level=0).sum()
     )
-    return CovidData(hosp_count, death_count, symptom_count)
-
-
-def _diff(start: Dict[str, Any], end: Dict[str, Any]) -> Dict[str, Any]:
-    return dict([(k, end[k] - start[k]) for k in end.keys()])
+    p = _get_data(hosp)
+    hosp_data = (
+        p[(p["datum"] == date_iso) & (p["geoRegion"] == "CHFL")]
+        .set_index("altersklasse_covid19")["sumTotal"]
+        .groupby(level=0).sum()
+    )
+    p = _get_data(symptoms)
+    symptom_data = (
+        p[
+            (p["date"] == at_date.isoformat())
+            & (p["geoRegion"] == "CHFL")
+            & (p["vaccine"] == "all")
+            & (p["age_group"] != "all")
+            & (p["severity"] == "all")
+        ]
+        .set_index("age_group")["sumTotal"]
+        .groupby(level=0).sum()
+    )
+    return CovidData(hosp_data, death_data, symptom_data)
 
 
 def _build_graph(
@@ -115,13 +107,12 @@ def _build_graph(
     start = _get_all_data(death, hosp, symptoms, START_DATE)
     end = _get_all_data(death, hosp, symptoms, END_DATE)
     counts = CovidData(
-        _diff(start.hosp, end.hosp),
-        _diff(start.death, end.death),
-        _diff(start.symptoms, end.symptoms),
+        end.hosp - start.hosp, end.death - start.death, end.symptoms - start.symptoms
     )
     plt.rcdefaults()
     fig, plot = plt.subplots()
     fig.suptitle(f"COVID / Vaccine comparison by age, {START_DATE} - {END_DATE}")
+    fig.set_size_inches(18.5, 10.5, forward=True)
     for age in counts.death.keys():
         death_count: int = counts.death[age]
         (y_1, y_2) = AGE_BAR_DICT[age]
@@ -175,24 +166,33 @@ def _build_graph(
     legends: List[Tuple[Tuple, str]] = [
         (HOSP_COLOR, f"COVID hospitalizations ({hosp['display_name']['en']})."),
         (DEATH_COLOR, f"COVID deaths ({death['display_name']['en']})."),
-        (SYMPTOM_COLOR, f"Reported vaccine adverse effects ({symptoms['display_name']['en']})."),
+        (
+            SYMPTOM_COLOR,
+            f"Reported vaccine adverse effects ({symptoms['display_name']['en']}).",
+        ),
     ]
-    if counts.death["Unbekannt"] > 0:
-        legends.append((DEATH_UNKNOWN_COLOR, "Death with unknown age."))
-    if counts.hosp["Unbekannt"] > 0:
-        legends.append((HOSP_UNKNOWN_COLOR, "Hospitalization with unknown age."))
-    if counts.symptoms["unknown"] > 0:
-        legends.append((SYMPTOM_UNKNOWN_COLOR, "Adverse effects with unknown age."))
-    legends.append((TRANSPARENT_COLOR,f"Data Source: opendata.swiss , package '{PACKAGE_NAME}'"))
-    legends.append((TRANSPARENT_COLOR,f"Data for CH + FL"))
-    legends.append((TRANSPARENT_COLOR,f"Code Source: https://github.com/deedf/covid_data"))
-    legends.append((TRANSPARENT_COLOR,f"Generated on {datetime.now().strftime('%Y-%m-%d')}"))
+    # if counts.death["age_group"]["Unbekannt"] > 0:
+    legends.append((DEATH_UNKNOWN_COLOR, "Death with unknown age."))
+    # if counts.hosp["age_group"]["Unbekannt"] > 0:
+    legends.append((HOSP_UNKNOWN_COLOR, "Hospitalization with unknown age."))
+    # if counts.symptoms["age_group"]["unknown"] > 0:
+    legends.append((SYMPTOM_UNKNOWN_COLOR, "Adverse effects with unknown age."))
+    legends.append(
+        (TRANSPARENT_COLOR, f"Data Source: opendata.swiss , package '{DATASET_NAME}'")
+    )
+    legends.append((TRANSPARENT_COLOR, "Data for CH + FL"))
+    legends.append(
+        (TRANSPARENT_COLOR, "Code Source: https://github.com/deedf/covid_data")
+    )
+    legends.append(
+        (TRANSPARENT_COLOR, "Generated on {datetime.now().strftime('%Y-%m-%d')}")
+    )
 
     fig.legend(
         [Rectangle(xy=(0, 0), width=10, height=10, color=l[0]) for l in legends],
         [l[1] for l in legends],
         loc="lower center",
-        fontsize="small"
+        fontsize="small",
     )
     plt.margins(0.1, 0.1)
     plot.spines["top"].set_visible(False)
@@ -202,15 +202,24 @@ def _build_graph(
 
 
 def _main() -> None:
-    packages = _get_package(PACKAGE_NAME)
-    resources_json: List[Dict[str, Any]] = packages["resources"]
-    resources_dict: Dict[str, Dict[str, Any]] = dict(
-        [(r["identifier"], r) for r in resources_json]
-    )
+    client = RemoteCKAN(CKAN_API_URL)
+    dataset = client.call_action("package_show", {"name_or_id": DATASET_NAME})
     _build_graph(
-        resources_dict["weekly-death-age-range-json"],
-        resources_dict["weekly-hosp-age-range-json"],
-        resources_dict["daily-vacc-symptoms-json"],
+        [
+            r
+            for r in dataset["resources"]
+            if r["identifier"] == "weekly-death-age-range-csv"
+        ][0],
+        [
+            r
+            for r in dataset["resources"]
+            if r["identifier"] == "weekly-hosp-age-range-csv"
+        ][0],
+        [
+            r
+            for r in dataset["resources"]
+            if r["identifier"] == "daily-vacc-symptoms-csv"
+        ][0],
     )
 
 
